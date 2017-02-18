9a3f99aff9f8bd07aa22eadbfb71d7f2143c6428
Aron Fyodor Asor <aronasorman@gmail.com>
2017-02-17 18:00:04 -0800

    Set the docker image for Kolibri as an artifact.

---
+++ .buildkite/build_dockerenv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+KOLIBRI_VERSION=`cat kolibri/VERSION`
+KOLIBRI_DOCKER_IMAGE_NAME=kolibri-$KOLIBRI_VERSION.dockerimg
+
+make dockerenvbuild
+docker image save learningequality/kolibri:$KOLIBRI_VERSION -o $KOLIBRI_DOCKER_IMAGE_NAME
+buildkite-agent artifact upload $KOLIBRI_DOCKER_IMAGE_NAME
--- .buildkite/build_whl.sh
+++ .buildkite/build_whl.sh
@@ -1,5 +1,7 @@
 l#!/usr/bin/env bash
 
+buildkite-agent artifact download '*.dockerimg'
+docker image load *.dockerimg
 make dockerenvdist
 buildkite-agent artifact upload 'dist/*.whl'
 buildkite-agent artifact upload 'dist/*.tar.gz'
